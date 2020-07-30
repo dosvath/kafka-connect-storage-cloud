6d728009fcf9acfafe5b42a5d45b273f567186e6
Daniel Osvath <dosvath@confluent.io>
2020-07-30 16:14:11 -0700

    CC-9723: DLQ WIP

--- kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
+++ kafka-connect-s3/src/main/java/io/confluent/connect/s3/S3SinkTask.java
@@ -22,6 +22,7 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.connect.errors.ConnectException;
 import org.apache.kafka.connect.errors.DataException;
 import org.apache.kafka.connect.errors.RetriableException;
+import org.apache.kafka.connect.sink.ErrantRecordReporter;
 import org.apache.kafka.connect.sink.SinkRecord;
 import org.apache.kafka.connect.sink.SinkTask;
 import org.apache.kafka.connect.sink.SinkTaskContext;
@@ -60,6 +61,7 @@ public class S3SinkTask extends SinkTask {
   private Format<S3SinkConnectorConfig, String> format;
   private RecordWriterProvider<S3SinkConnectorConfig> writerProvider;
   private final Time time;
+  private ErrantRecordReporter reporter;
 
   /**
    * No-arg constructor. Used by Connect framework.
@@ -115,6 +117,16 @@ public class S3SinkTask extends SinkTask {
       partitioner = newPartitioner(connectorConfig);
 
       open(context.assignment());
+      try {
+        reporter = context.errantRecordReporter();
+        if (reporter == null) {
+          log.info("Errant record reporter not configured.");
+        }
+      } catch (NoClassDefFoundError e) {
+        // Will occur in Connect runtimes earlier than 2.6
+        log.warn("AK versions prior to 2.6 do not support the errant record reporter");
+      }
+
       log.info("Started S3 connector task with assigned partitions: {}",
           topicPartitionWriters.keySet()
       );
@@ -204,6 +216,9 @@ public class S3SinkTask extends SinkTask {
       TopicPartition tp = new TopicPartition(topic, partition);
 
       if (maybeSkipOnNullValue(record)) {
+        if (reporter != null) {
+          reporter.report(record, new ConnectException("Cannot write null value record"));
+        }
         continue;
       }
       topicPartitionWriters.get(tp).buffer(record);
